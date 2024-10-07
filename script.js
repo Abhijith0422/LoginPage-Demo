6f6eed623a46fde51513ffc8a13123fef716fdf4
Abhijith R Nair <aswajith679@gmail.com>
2024-10-07 21:45:31 +0530

    edit to null

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 
 var username = document.getElementById('username');
     var password = document.getElementById('password');
-    
+    var errorMessage = document.getElementById('error-message');
 
 document.getElementById('login').addEventListener("submit",function(event){
     event.preventDefault();
-    
-    console.log('Username: ' + username.value + ' Password: ' + password.value);
-if (username.value == 'admin' && password.value == '123456789') {
-    alert('Login Successful');
+    errorMessage.textContent = '';
+  if(username.value!='' && password.value!=''){ 
+if (username.value == 'Abhijith' && password.value == 'Abhijith@0422') {
+    alert('Admin Login Successful');
     window.location.href='loginpage.html';
 }
+    else{
+        errorMessage.innerHTML = "<i class='bx bxs-info-circle'></i> Invalid credentials. Please enter valid credentials.";
+    console.log('Username: ' + username.value + ' Password: ' + password.value);
+    }}
+    else{
+        errorMessage.innerHTML = "<i class='bx bxs-info-circle'></i>Please fill in both username and password.";
+    }
 });
 
 
--- signupscript.js
+++ signupscript.js
@@ -2,16 +2,20 @@ var emailid = document.getElementById('email');
     var username = document.getElementById('userid');
     var password = document.getElementById('password');
     var confirmpassword = document.getElementById('confirmpass');
+    var errorMessage = document.getElementById('error-message');
 
 document.getElementById('signup').addEventListener("submit",function(event){
     event.preventDefault();
     
-    
+    if(emailid.value != '' && username.value !=''&&password.value != ''){
     if (password.value == confirmpassword.value) {
         console.log('Email ID: ' + emailid.value + ' Username: ' + username.value + ' Password: ' + password.value);
         alert('Signup Successful');
         window.location.href='signupsucesspage.html';
-        
+    }
+    }
+    else{
+        errorMessage.innerHTML = "<i class='bx bxs-info-circle'></i> Please fill in all the fields.";
     }
 
 });
